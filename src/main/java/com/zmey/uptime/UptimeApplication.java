ee07452ecd1c7440db88d09b31b3f096dfbfb842
Victor <devitos76@gmial.com>
2025-09-17 23:06:01 +0300

    ApplicationPidFileWriter

--- src/main/java/com/zmey/uptime/UptimeApplication.java
+++ src/main/java/com/zmey/uptime/UptimeApplication.java
@@ -2,6 +2,7 @@ package com.zmey.uptime;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
@@ -9,7 +10,9 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class UptimeApplication extends AbstractApplication {
 
 	public static void main(String[] args) {	
-		SpringApplication.run(UptimeApplication.class, args);
+		SpringApplication app = new SpringApplication(UptimeApplication.class);
+		app.addListeners(new ApplicationPidFileWriter());
+		app.run(args);
 	}
 
 }
