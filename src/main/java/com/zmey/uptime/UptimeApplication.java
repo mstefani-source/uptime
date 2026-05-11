993b7acdfa1c645e4d9c4935d9da7bd9fa0d1fa1
Ivanov Victor <devitos76@gmail.com>
2026-05-11 22:14:17 +0300

    modified:   src/main/java/com/zmey/uptime/UptimeApplication.java

--- src/main/java/com/zmey/uptime/UptimeApplication.java
+++ src/main/java/com/zmey/uptime/UptimeApplication.java
@@ -9,7 +9,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 public class UptimeApplication extends AbstractApplication {
 
-	public static void main(String[] args) {	
+	public static void main(String[] args) {
 		SpringApplication app = new SpringApplication(UptimeApplication.class);
 		app.addListeners(new ApplicationPidFileWriter());
 		app.run(args);
